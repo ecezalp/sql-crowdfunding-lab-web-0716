a82a9a6e460050084a90e3e855b3cdd1b7cb680e
Ece Ozalp <github email address>
2016-08-04 15:57:03 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,86 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("pebble watch", "technology", 1000000, 2015-12-15, 2016-12-15);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("coolest cooler", "technology", 500000, 2016-06-06, 2016-07-06);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("exploding kittens", "recreation", 50000, 2015-01-04, 2016-01-04);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("Pono Music", "technology", 100000, 2014-07-04, 2015-07-04);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("the veronica mars movie project", "entertainment", 1000000, 2015-03-03, 2016-04-03);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("reading rainbow", "educational", 500000, 2015-06-03, 2016-06-03);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("mighty no 9", "entertainment", 200000, 2015-04-06, 2016-04-06);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("the micro", "technology", 800000, 2015-01-01, 2016-01-01);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("wasteland 2", "entertainment", 300000, 2015-02-05, 2016-02-05);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date)
+VALUES ("sense", "health", 100000, 2015-01-02, 2016-01-02); 
+
+
+INSERT INTO users (name, age)
+VALUES ("Dillon McRoberts", 40),
+("Andrew J Klein", 10),
+("Charlie Cook", 22),
+("Ece Ozalp", 26),
+("Gentian Bardhoshi", 23),
+("Jasper Curry", 25),
+("Abigail Diaz", 66),
+("Colby Schulz", 34),
+("Tami Jaton", 56),
+("Ali Mullin", 43),
+("Tony Mai", 43),
+("Schyler Hosler", 43),
+("Daniella Penn", 43),
+("Alexandria Sanborn", 43),
+("Troy Chryssos", 43),
+("Francis Rocco", 43),
+("Clifton (Cliff) Regis", 43),
+("Alice Kallaugher", 43),
+("Mahati Gollamudi", 43),
+("Mary Boyd", 43);
+
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (3000, 3, 5),
+(400, 1, 9),
+(600, 18, 1),
+(700, 13, 5),
+(815, 4, 2),
+(20, 8, 3),
+(50000, 4, 6),
+(70000, 12, 4),
+(60000, 3, 2),
+(2300, 14, 4),
+(50000, 12, 9),
+(3400, 11, 8),
+(12000, 3, 1),
+(60000, 5, 1),
+(90000, 18, 6),
+(30000, 1, 10),
+(90000, 2, 2),
+(5400, 2, 9),
+(50000, 6, 7),
+(600000, 3, 9),
+(2000, 12, 4),
+(2000, 4, 10),
+(2000, 12, 7),
+(2000, 9, 8),
+(2000, 20, 1),
+(2000, 7, 5),
+(2000, 2, 6),
+(2000, 6, 4),
+(2000, 6, 10),
+(2000, 2, 9);
+
--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,31 +1,66 @@
-# Write your sql queries in this file in the appropriate method like the example below:
-#
-# def select_category_from_projects
-# "SELECT category FROM projects;"
-# end
-
-# Make sure each ruby method returns a string containing a valid SQL statement.
-
 def selects_the_titles_of_all_projects_and_their_pledge_amounts
-"Write your SQL query Here"
+  <<-SQL
+    SELECT projects.title as project_title, SUM(pledges.amount) as total_pledge_amount 
+    FROM projects 
+    JOIN pledges
+    ON projects.id = pledges.project_id
+    GROUP BY projects.title;
+  SQL
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges
-"Write your SQL query Here"
+  <<-SQL
+    SELECT users.name as user_name, users.age as users_age, SUM(pledges.amount) as total_pledge_amount 
+    FROM users
+    LEFT JOIN pledges
+    ON users.id = pledges.user_id
+    GROUP BY users.name;
+  SQL
+
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+<<-SQL
+    SELECT projects.title as project_title, (SUM(pledges.amount) - projects.funding_goal) as amount_over_goal 
+    FROM projects
+    JOIN pledges
+    ON projects.id = pledges.project_id
+    GROUP BY projects.title
+    HAVING SUM(pledges.amount) >= projects.funding_goal;
+
+  SQL
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount
-"Write your SQL query Here"
+<<-SQL
+    SELECT users.name as user_name, SUM(pledges.amount) as total_amount
+    FROM users
+    JOIN pledges
+    ON users.id = pledges.user_id
+    GROUP BY users.name
+    ORDER BY total_amount ASC;
+  SQL
+
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+<<-SQL
+    SELECT projects.category, pledges.amount as total_amount
+    FROM projects
+    JOIN pledges
+    ON projects.id = pledges.project_id
+    WHERE projects.category = "music"; 
+  SQL
+
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_book_category
-"Write your SQL query Here"
+<<-SQL
+    SELECT projects.category, SUM(pledges.amount) as total_amount
+    FROM projects
+    JOIN pledges
+    ON projects.id = pledges.project_id
+    WHERE projects.category = "books"
+    GROUP BY projects.category; 
+  SQL
 end
